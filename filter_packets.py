ce2cef2485c9af8f2889bd8bf6228dffb7f2a602
Michael Vasile <michaelvasile17@gmail.com>
2019-12-06 16:02:52 -0500

    Changed filtering script to correct output

--- filter_packets.py
+++ filter_packets.py
@@ -1,18 +1,30 @@
 # Parameters: File name to parse, protocol to search for (ex. "ICMP")
 def filter(inputFileName, protocol):
-	print 'called filter function in filter_packets.py'
-	outputFileName = inputFileName.split(".")[0] + "_filtered.txt"
-	print outputFileName
-	
-	fIn = open(inputFileName, 'r')
-	fOut = open(outputFileName, 'w')
-	line = fIn.readline()
-	
-	while line:
-                lineSplit = line.split()
-                for a in lineSplit:
-                        if(a == protocol):
-                                for x in lineSplit:
-                                        fOut.write(x.replace(",", "") + ";")
-                                fOut.write("\n")
-                line = fIn.readline()
+   print 'called filter function in filter_packets.py'
+   
+   # Create an output file based on the input filename
+   outputFileName = inputFileName.split(".")[0] + "_filtered.txt"
+   print(outputFileName)
+
+   # Open the input and output files
+   fIn = open(inputFileName, 'r')
+   fOut = open(outputFileName, 'w')
+   
+   a = str()
+   b = list()
+
+   for line in fIn:
+      if line[0] == 'N':
+         b += [a]
+         a = line
+      else:
+         a += line
+
+   for packet in b:
+      if packet.find('ICMP') != -1:
+         fOut.write(packet)
+      else:
+         continue
+
+   fIn.close()
+   fOut.close()
