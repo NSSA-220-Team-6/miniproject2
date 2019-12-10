d645d8fba52c82ad00c84d8250757173ea0e50d5
Michael Vasile <5721500+MichaelVasile@users.noreply.github.com>
2019-12-10 00:09:28 -0500

    Update filter_packets.py

--- filter_packets.py
+++ filter_packets.py
@@ -1,23 +1,34 @@
-from filter_packets import *
-from packet_parser import *
-from compute_metrics import *
+# Parameters: File name to parse, protocol to search for (ex. "ICMP")
+def filter(inputFileName, protocol):
+   print 'called filter function in filter_packets.py'
+   
+   # Create an output file based on the input filename
+   outputFileName = inputFileName.split(".")[0] + "_filtered.txt"
 
-files = ['Node1.txt', 'Node2.txt', 'Node3.txt', 'Node4.txt']
+   # Open the input and output files
+   fIn = open(inputFileName, 'r')
+   fOut = open(outputFileName, 'w')
 
-for f in files:
+   # Variables to store information   
+   a = str()
+   b = list()
 
-   ip = str()
-   # Get node IP address
-   if f == "Node1.txt":
-      ip = "192.168.100.1"
-   elif f == "Node2.txt":
-      ip = "192.168.100.2"
-   elif f == "Node3.txt":
-      ip = "192.168.200.1"
-   elif f == "Node4.txt":
-      ip = "192.168.200.2" 
+   # Iterate through input file to separate packets into a list
+   for line in fIn:
+      if line[0] == 'N':
+         b += [a] # Store the current line
+         a = line # set a to current line
+      else:
+         a += line # append packet information to string
 
-   filter(f, "ICMP")
-   filtered = f.split(".")[0] + "_filtered.txt"
-   L = parse(filtered)
-   compute(L, ip)
+   # Indentify and filter packets by specified protocol
+   for packet in b:
+      # Check if list index contains protocol in string (!= -1 meaning it has been found) 
+      # Also check if the packet does not contain "unreachable" as part of an error message (== -1 meaning it has not been found)
+      if packet.find(protocol) != -1 and packet.find("unreachable") == -1:
+         # Write packet to output file if found
+         fOut.write(packet)
+
+   # Close files
+   fIn.close()
+   fOut.close()
