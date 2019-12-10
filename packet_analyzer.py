c23b92d67b02425210c71ac2220829e30c0ad042
Jared Marcuccilli <jaredmarcuccilli@users.noreply.github.com>
2019-12-09 22:40:44 -0500

    Update packet_analyzer.py

--- packet_analyzer.py
+++ packet_analyzer.py
@@ -2,7 +2,7 @@ from filter_packets import *
 from packet_parser import *
 from compute_metrics import *
 
-files = ["Node1.txt"]
+files = ["Node1.txt", "Node2.txt", "Node3.txt", "Node4.txt"]
 for file in files:
     filter(file, "ICMP")
     L = parse(file.split(".")[0] + "_filtered.txt")
