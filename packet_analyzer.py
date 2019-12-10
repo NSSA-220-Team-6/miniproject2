391c126c7dfb678317abf81826ce50cda1cfd099
Michael Vasile <5721500+MichaelVasile@users.noreply.github.com>
2019-12-10 00:09:51 -0500

    Update packet_analyzer.py

--- packet_analyzer.py
+++ packet_analyzer.py
@@ -2,8 +2,22 @@ from filter_packets import *
 from packet_parser import *
 from compute_metrics import *
 
-files = ["Node1.txt", "Node2.txt", "Node3.txt", "Node4.txt"]
-for file in files:
-    filter(file, "ICMP")
-    L = parse(file.split(".")[0] + "_filtered.txt")
-    compute(L, file)
+files = ['Node1.txt', 'Node2.txt', 'Node3.txt', 'Node4.txt']
+
+for f in files:
+
+   ip = str()
+   # Get node IP address
+   if f == "Node1.txt":
+      ip = "192.168.100.1"
+   elif f == "Node2.txt":
+      ip = "192.168.100.2"
+   elif f == "Node3.txt":
+      ip = "192.168.200.1"
+   elif f == "Node4.txt":
+      ip = "192.168.200.2" 
+
+   filter(f, "ICMP")
+   filtered = f.split(".")[0] + "_filtered.txt"
+   L = parse(filtered)
+   compute(L, ip)
