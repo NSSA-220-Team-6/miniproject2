62735a80822f9f28ee3919157c5c600a5d269998
Michael Vasile <5721500+MichaelVasile@users.noreply.github.com>
2019-12-10 00:08:20 -0500

    Update filter_packets.py

--- filter_packets.py
+++ filter_packets.py
@@ -1,34 +1,23 @@
-# Parameters: File name to parse, protocol to search for (ex. "ICMP")
-def filter(inputFileName, protocol):
-   print ('called filter function in filter_packets.py')
-   
-   # Create an output file based on the input filename
-   outputFileName = inputFileName.split(".")[0] + "_filtered.txt"
+from filter_packets import *
+from packet_parser import *
+from compute_metrics import *
 
-   # Open the input and output files
-   fIn = open(inputFileName, 'r')
-   fOut = open(outputFileName, 'w')
+files = ['Node1.txt', 'Node2.txt', 'Node3.txt', 'Node4.txt']
 
-   # Variables to store information   
-   a = str()
-   b = list()
+for f in files:
 
-   # Iterate through input file to separate packets into a list
-   for line in fIn:
-      if line[0] == 'N':
-         b += [a] # Store the current line
-         a = line # set a to current line
-      else:
-         a += line # append packet information to string
+   ip = str()
+   # Get node IP address
+   if f == "Node1.txt":
+      ip = "192.168.100.1"
+   elif f == "Node2.txt":
+      ip = "192.168.100.2"
+   elif f == "Node3.txt":
+      ip = "192.168.200.1"
+   elif f == "Node4.txt":
+      ip = "192.168.200.2" 
 
-   # Indentify and filter packets by specified protocol
-   for packet in b:
-      # Check if list index contains protocol in string (!= -1 meaning it has been found) 
-      # Also check if the packet does not contain "unreachable" as part of an error message (== -1 meaning it has not been found)
-      if packet.find(protocol) != -1 and packet.find("unreachable") == -1:
-         # Write packet to output file if found
-         fOut.write(packet)
-
-   # Close files
-   fIn.close()
-   fOut.close()
+   filter(f, "ICMP")
+   filtered = f.split(".")[0] + "_filtered.txt"
+   L = parse(filtered)
+   compute(L, ip)
